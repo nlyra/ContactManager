9ddd4de12fcb26cc0f00ed5aef4b80245066c886
Nathaniel Lyra <nlyra@knights.ucf.edu>
2020-08-31 21:02:51 -0400

    Basic SignUp API endpoint complete.

---
+++ LAMPAPI/SignUp.php
@@ -0,0 +1,44 @@
+<?php
+
+    $inData = getRequestInfo();
+
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $email = $inData["email"];
+    $password = $inData["password"];
+
+    $conn = new mysqli("localhost", "nathaniellyra", "hello123", "contactmanager_database");
+
+    if ($conn->connect_error)
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        $sql = "INSERT into user (FirstName, LastName, Password, Email) VALUES (' $firstName ', ' $lastName ', ' $password ', ' $email ')";
+
+        if ( $result = $conn->query($sql) != TRUE)
+        {
+            returnWithError( $conn->error );
+        }
+
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"error:"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-Type: application/json');
+        echo $obj;
+    }
+?>
