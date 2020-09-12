e94cf5b763298db65dbd42894e11ca51cb6e730d
javy3555 <54584971+javy3555@users.noreply.github.com>
2020-09-12 12:58:24 -0400

    Add files via upload

--- devinSmallProject/js/script.js
+++ devinSmallProject/js/script.js
@@ -31,7 +31,7 @@ function doLogin()
 		var jsonObject = JSON.parse( xhr.responseText );
 
 		userId = jsonObject.id;
-
+		console.log(userId);
 		if( userId < 1 )
 		{
 			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
